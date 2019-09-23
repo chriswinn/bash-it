75b3392c20b3c5c4afa9f682915e7ca8fa502a29
Chris Winn <chriswinn@gmail.com>
2019-09-23 00:21:57 -0400

    adding basic c17n.theme.bash file.

--- themes/c17n/c17n.theme.bash
+++ themes/c17n/c17n.theme.bash
@@ -13,11 +13,9 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
-THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"false"}
 THEME_SHOW_CLOCK=${THEME_SHOW_CLOCK:-"true"}
 THEME_SHOW_VENV=${THEME_SHOW_VENV:-"true"}
-THEME_SHOW_VENV_RUBY=${THEME_SHOW_VENV_RUBY:-"false"} # Defaults to Python, unless this == "true"
-THEME_SHOW_BATTERY=${THEME_SHOW_BATTERY:-"true"}
+THEME_SHOW_VENV_RUBY=${THEME_SHOW_VENV_RUBY:-"true"} # Defaults to Python, unless this == "true"
 THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
 THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
 THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M"}
@@ -26,30 +24,19 @@ __c17n_clock() {
   if [[ "${THEME_SHOW_CLOCK}" == "true" ]]; then
 		printf "$(clock_prompt)" && __c17n_space 
   fi
-	if [[ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]]; then
-		printf "$(clock_char) "
-	fi
 }
 
 __c17n_venv() {
   if [[ "${THEME_SHOW_VENV}" == "true" ]] && [[ "${THEME_SHOW_VENV_RUBY}" == "true" ]]; then
     printf "($(ruby_version_prompt))" && __c17n_space 
-	else 
+	elif [[ "${THEME_SHOW_VENV}" == "true" ]]; then  
     printf "($(python_version_prompt))" && __c17n_space
   fi
 }
 
-__c17n_space() {
-  printf ' ' 
-}
-
 function prompt_command() {
-    #bobby_theme#PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
-  if [[ "${THEME_SHOW_BATTERY}" == "true" ]]; then
+		#PS1="\n$(__c17n_clock)${yellow}$(__c17n_venv)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
     PS1="\n$(battery_char) $(__c17n_clock)${yellow}$(__c17n_venv)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
-	else 
-		PS1="\n$(__c17n_clock)${yellow}$(__c17n_venv)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
-  fi 
 }
 
 safe_append_prompt_command prompt_command
