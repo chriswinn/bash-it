ff5065032134b2fe619e97a834c192a841a841a0
Chris Winn <chriswinn@gmail.com>
2019-09-22 20:54:04 -0400

    initial addition of the c17n theme folder & file.

---
+++ themes/c17n/c17n.theme.bash
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${green}|"
+SCM_THEME_PROMPT_SUFFIX="${green}|"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${green}|"
+GIT_THEME_PROMPT_SUFFIX="${green}|"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"false"}
+THEME_SHOW_CLOCK=${THEME_SHOW_CLOCK:-"true"}
+THEME_SHOW_VENV=${THEME_SHOW_VENV:-"true"}
+THEME_SHOW_VENV_RUBY=${THEME_SHOW_VENV_RUBY:-"false"} # Defaults to Python, unless this == "true"
+THEME_SHOW_BATTERY=${THEME_SHOW_BATTERY:-"true"}
+THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M"}
+
+__c17n_clock() {
+  if [[ "${THEME_SHOW_CLOCK}" == "true" ]]; then
+		printf "$(clock_prompt)" && __c17n_space 
+  fi
+	if [[ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]]; then
+		printf "$(clock_char) "
+	fi
+}
+
+__c17n_venv() {
+  if [[ "${THEME_SHOW_VENV}" == "true" ]] && [[ "${THEME_SHOW_VENV_RUBY}" == "true" ]]; then
+    printf "($(ruby_version_prompt))" && __c17n_space 
+	else 
+    printf "($(python_version_prompt))" && __c17n_space
+  fi
+}
+
+__c17n_space() {
+  printf ' ' 
+}
+
+function prompt_command() {
+    #bobby_theme#PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+  if [[ "${THEME_SHOW_BATTERY}" == "true" ]]; then
+    PS1="\n$(battery_char) $(__c17n_clock)${yellow}$(__c17n_venv)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+	else 
+		PS1="\n$(__c17n_clock)${yellow}$(__c17n_venv)${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+  fi 
+}
+
+safe_append_prompt_command prompt_command
